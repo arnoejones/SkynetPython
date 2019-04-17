c1d200110e9758e7226f2dde70b377a0016d80a6
Arno Jones <arno.jones@igt.com>
2019-04-17 07:27:59 -0700

    Got a working request from sql to display on webpage

--- skynet.py
+++ skynet.py
@@ -3,29 +3,13 @@ from SkynetSqlConnect import getData
 
 app = Flask(__name__)
 
-# simulate a db call
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Arno Jones',
-        'title': 'Blog Post 2',
-        'content': 'My first post content',
-        'date_posted': 'April 21, 2018'
-    },
-]
-
 results = getData()
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=results)
-    # print(posts)
+
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
